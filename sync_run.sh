5fe29c575f2e770d0fd2ca03813dcdc7ba86a1a5
Jichan Chung <jichan3751@blaze.Millennium.Berkeley.EDU>
2020-12-04 02:42:41 -0800

    added slurm run

--- sync_run.sh
+++ sync_run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+#SBATCH --nodelist=steropes # if you need specific nodes
+#SBATCH -N 1 # number of nodes requested
+#SBATCH -n 1 # number of tasks (i.e. processes)
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH -t 2-00:00 # time requested (D-HH:MM)
+#SBATCH --output=slurm.out.txt
+#SBATCH --error=slurm.err.txt
+
+
 # DEST: end with '/'
-DEST="$HOME/pp/tmp/201204_tmp/"
+DEST="/data/$USER/tmp/201204_tmp/"
 
 # sync to destination dir
 mkdir -p ${DEST}
